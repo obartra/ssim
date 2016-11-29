8dca9c4351fb1328f7471d1380eee59c0615ac9c
Oscar <obartra@gmail.com>
2016-11-28 16:54:26 -0800

    ci(reporting): Fix Incorrect Test Coverage Report
    Since switching to the parallel build test coverage is not reported correctly. It's still enforced
    at 100% but the reported values to codeclimate are 17%

--- ci/node0.sh
+++ ci/node0.sh
@@ -2,12 +2,7 @@
 #
 # Runs testing tasks on first CircleCI node
 #
-# Coverage related tasks are grouped together because they all depend on the output of
-# `npm run cover`.
 
 echo "Running node $CIRCLE_NODE_INDEX"
 
 npm run cover                         # Report test coverage locally
-npm run cover:check                   # Fail if coverage drops below 100%
-npm run codeclimate                   # Run tests and send coverage to code climate
-cp -R coverage/* $CIRCLE_TEST_REPORTS # Copy test coverage reports for CircleCI
