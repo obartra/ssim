7a335471801811b417deeec629fd87c78074a3f1
Oscar <oscar.bartra@paperg.com>
2017-01-25 16:29:52 -0800

    test(performance): Improve performance tests 💫 (#86)
    Small refactor of performance tests to reduce duplication

--- spec/perf/index.spec.js
+++ spec/perf/index.spec.js
@@ -10,7 +10,9 @@ const oldSSIM = niv.require('ssim.js@latest');
 
 const samples = {
 	avion: join(__dirname, '../samples/IVC_SubQualityDB/color/avion.bmp'),
-	avion_j2000_r1: join(__dirname, '../samples/IVC_SubQualityDB/color/avion_j2000_r1.bmp')
+	avion_j2000_r1: join(__dirname, '../samples/IVC_SubQualityDB/color/avion_j2000_r1.bmp'),
+	large: join(__dirname, '../samples/aspectratio/1.jpg'),
+	large_high: join(__dirname, '../samples/aspectratio/1_high.jpg')
 };
 
 const method = {
@@ -20,20 +22,27 @@ const method = {
 	SLOW: 'Original SSIM'
 };
 
+function onError(methodName, t) {
+	return () => {
+		t.fail(`${methodName} failed`);
+		t.end();
+	};
+}
+
 test('ssim should be faster than originalSsim', (t) => {
 	measure(t, 'SSIM algorithm', true, {
 		name: method.FAST,
 		fn(deferred) {
 			ssim(samples.avion, samples.avion_j2000_r1, { ssim: 'fast' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.FAST} failed`));
+				.catch(onError(method.FAST, t));
 		}
 	}, {
 		name: method.SLOW,
 		fn(deferred) {
 			ssim(samples.avion, samples.avion_j2000_r1, { ssim: 'original' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.SLOW} failed`));
+				.catch(onError(method.SLOW, t));
 		}
 	});
 });
@@ -44,32 +53,50 @@ test('bezkrovny ssim should be faster than ssim', (t) => {
 		fn(deferred) {
 			ssim(samples.avion, samples.avion_j2000_r1, { ssim: 'bezkrovny' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.FAST} failed`));
+				.catch(onError(method.BEZKROVNY, t));
 		}
 	}, {
 		name: method.FAST,
 		fn(deferred) {
 			ssim(samples.avion, samples.avion_j2000_r1, { ssim: 'fast' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.SLOW} failed`));
+				.catch(onError(method.FAST, t));
 		}
 	});
 });
 
-test('ssim shouldn\'t be slower than the last published version of SSIM', (t) => {
+test('ssim shouldn\'t be slower than the last published version of SSIM  for small images', (t) => {
 	measure(t, 'SSIM regression', true, {
 		name: method.FAST,
 		fn(deferred) {
 			ssim(samples.avion, samples.avion_j2000_r1, { ssim: 'fast' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.FAST} failed`));
+				.catch(onError(method.FAST, t));
 		}
 	}, {
 		name: method.PRIOR,
 		fn(deferred) {
 			oldSSIM(samples.avion, samples.avion_j2000_r1, { ssim: 'fast' })
 				.then(() => deferred.resolve())
-				.catch(() => t.fail(`${method.PRIOR} failed`));
+				.catch(onError(method.PRIOR, t));
+		}
+	});
+});
+
+test('ssim shouldn\'t be slower than the last published version of SSIM for large images', (t) => {
+	measure(t, 'large image SSIM regression', true, {
+		name: method.FAST,
+		fn(deferred) {
+			ssim(samples.large, samples.large_high, { ssim: 'fast' })
+				.then(() => deferred.resolve())
+				.catch(onError(method.FAST, t));
+		}
+	}, {
+		name: method.PRIOR,
+		fn(deferred) {
+			oldSSIM(samples.large, samples.large_high, { ssim: 'fast' })
+				.then(() => deferred.resolve())
+				.catch(onError(method.PRIOR, t));
 		}
 	});
 });
