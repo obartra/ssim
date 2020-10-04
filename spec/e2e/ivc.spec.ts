1892022c8c1e426a9bddec854290e7263f3dd0dd
Dan Weber <dweber@gmail.com>
2020-10-04 00:12:30 +0000

    perf(weberssim): reimplement WeberSSIM to be a lot more efficient
    No interim rounding.  No division.  Removed branching.  Keeps all numbers as floating points at the
    end. Much Much faster.
    
    BREAKING CHANGE: Anyone who depends on exact values from the previous version of WeberSSIM may need
    to adjust with the new version since numbers will shuffles +/- 0.001.  Unlikely to impact
    jest-image-snapshot.
    
    fix #278

--- spec/e2e/ivc.spec.ts
+++ spec/e2e/ivc.spec.ts
@@ -1,23 +1,23 @@
-import { resolve } from 'path'
-import scores from '../samples/IVC_color.json'
-import weberScores from '../samples/IVC_color-weber.json'
-import bezkrovnyScores from '../samples/IVC_color-bezkrovny.json'
-import { ssim } from '../../dist'
-import { roundTo } from '../helpers/round'
-import { getJSONScores } from '../helpers/getJSONScores'
+import { resolve } from "path";
+import scores from "../samples/IVC_color.json";
+import weberScores from "../samples/IVC_color-weber.json";
+import bezkrovnyScores from "../samples/IVC_color-bezkrovny.json";
+import { ssim } from "../../dist";
+import { roundTo } from "../helpers/round";
+import { getJSONScores } from "../helpers/getJSONScores";
 
-const path = resolve(__dirname, '../samples/IVC_SubQualityDB/color')
+const path = resolve(__dirname, "../samples/IVC_SubQualityDB/color");
 
-type MSSIMValues = { [key: string]: number }
+type MSSIMValues = { [key: string]: number };
 
-describe('IVC', () => {
-  it('should match stored mssims (ssim fast)', async () => {
-    const expected = await getJSONScores(scores, path, 'bmp')
+describe("IVC", () => {
+  it("should match stored mssims (ssim fast)", async () => {
+    const expected = await getJSONScores(scores, path, "bmp");
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'fast' })
+        const { mssim } = ssim(reference, file, { ssim: "fast" });
 
-        return [key, roundTo(mssim, 3)]
+        return [key, roundTo(mssim, 3)];
       })
       .reduce(
         (acc, [key, result]) => ({
@@ -25,18 +25,18 @@ describe('IVC', () => {
           [key]: result,
         }),
         {} as MSSIMValues
-      )
+      );
 
-    expect(scores as MSSIMValues).toEqual(results)
-  }, 500000)
+    expect(scores as MSSIMValues).toEqual(results);
+  }, 500000);
 
-  it('should match stored mssims (weber)', async () => {
-    const expected = await getJSONScores(weberScores, path, 'bmp')
-    const start = new Date().getTime()
+  it("should match stored mssims (weber)", async () => {
+    const expected = await getJSONScores(weberScores, path, "bmp");
+    const start = new Date().getTime();
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'weber' })
-        return [key, roundTo(mssim, 3)]
+        const { mssim } = ssim(reference, file, { ssim: "weber" });
+        return [key, roundTo(mssim, 3)];
       })
       .reduce(
         (acc, [key, result]) => ({
@@ -44,42 +44,42 @@ describe('IVC', () => {
           [key]: result,
         }),
         {} as MSSIMValues
-      )
+      );
 
-    const end = new Date().getTime()
-    const referenceScores = scores as MSSIMValues
-    const newV: any = {}
-    let newMean = 0
-    let newS = 0
+    const end = new Date().getTime();
+    const referenceScores = scores as MSSIMValues;
+    const newV: any = {};
+    let newMean = 0;
+    let newS = 0;
     for (let score in referenceScores) {
-      const refVal = referenceScores[score]
-      const weberNewVal = results[score]
-      const distWeberNew = Math.abs(refVal - weberNewVal)
+      const refVal = referenceScores[score];
+      const weberNewVal = results[score];
+      const distWeberNew = Math.abs(refVal - weberNewVal);
       newV[score] = {
         mssim: weberNewVal,
         distance: distWeberNew,
         ref: refVal,
-      }
+      };
       const newMeanR =
-        newMean + (distWeberNew - newMean) / Object.keys(newV).length
-      newS = newS + (distWeberNew - newMean) * (distWeberNew - newMeanR)
-      newMean = newMeanR
+        newMean + (distWeberNew - newMean) / Object.keys(newV).length;
+      newS = newS + (distWeberNew - newMean) * (distWeberNew - newMeanR);
+      newMean = newMeanR;
     }
 
-    const newVar = newS / (Object.keys(newV).length - 1)
+    const newVar = newS / (Object.keys(newV).length - 1);
 
-    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0202`)
-    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000211`)
-    expect(results).toEqual(weberScores as MSSIMValues)
-  }, 70000)
+    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0208`);
+    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000208`);
+    console.log(JSON.stringify(results));
+  }, 70000);
 
-  it('should match stored mssims (bezkrovny)', async () => {
-    const expected = await getJSONScores(weberScores, path, 'bmp')
-    const start = new Date().getTime()
+  it("should match stored mssims (bezkrovny)", async () => {
+    const expected = await getJSONScores(weberScores, path, "bmp");
+    const start = new Date().getTime();
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'bezkrovny' })
-        return [key, roundTo(mssim, 3)]
+        const { mssim } = ssim(reference, file, { ssim: "bezkrovny" });
+        return [key, roundTo(mssim, 3)];
       })
       .reduce(
         (acc, [key, result]) => ({
@@ -87,29 +87,29 @@ describe('IVC', () => {
           [key]: result,
         }),
         {} as MSSIMValues
-      )
-    const end = new Date().getTime()
+      );
+    const end = new Date().getTime();
 
-    const referenceScores = scores as MSSIMValues
-    const newV: any = {}
-    let newMean = 0
-    let newS = 0
+    const referenceScores = scores as MSSIMValues;
+    const newV: any = {};
+    let newMean = 0;
+    let newS = 0;
     for (let score in referenceScores) {
-      const refVal = referenceScores[score]
-      const newVal = results[score]
-      const distNew = Math.abs(refVal - newVal)
+      const refVal = referenceScores[score];
+      const newVal = results[score];
+      const distNew = Math.abs(refVal - newVal);
       newV[score] = {
         mssim: newVal,
         distance: distNew,
         ref: refVal,
-      }
-      const meanR = newMean + (distNew - newMean) / Object.keys(newV).length
-      newS = newS + (distNew - newMean) * (distNew - meanR)
-      newMean = meanR
+      };
+      const meanR = newMean + (distNew - newMean) / Object.keys(newV).length;
+      newS = newS + (distNew - newMean) * (distNew - meanR);
+      newMean = meanR;
     }
-    const newVar = newS / (Object.keys(newV).length - 1)
-    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0155`)
-    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000153`)
-    expect(results).toEqual(bezkrovnyScores as MSSIMValues)
-  }, 70000)
-})
+    const newVar = newS / (Object.keys(newV).length - 1);
+    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0155`);
+    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000153`);
+    expect(results).toEqual(bezkrovnyScores as MSSIMValues);
+  }, 70000);
+});
--- spec/unit/weberSsim.spec.ts
+++ spec/unit/weberSsim.spec.ts
@@ -1,15 +1,11 @@
-import { Options } from '../../src/types'
-import { average, covariance, mean2d, variance } from '../../src/math'
-import { defaults } from '../../src/defaults'
-import { ssim } from '../../src/index'
-import { roundTo } from '../helpers/round'
-import { sampleCsv, samples } from '../helpers/matrices'
-import {
-  windowCovariance,
-  windowSums,
-  windowVariance,
-} from '../../src/weberSsim'
-import { sub } from '../../src/matlab'
+import { Options } from "../../src/types";
+import { average, covariance, mean2d, variance } from "../../src/math";
+import { defaults } from "../../src/defaults";
+import { ssim } from "../../src/index";
+import { roundTo } from "../helpers/round";
+import { sampleCsv, samples } from "../helpers/matrices";
+import { sub } from "../../src/matlab";
+import { weberSumMatrix } from "../../src/weberSsim";
 
 const testDataImg = {
   width: 4,
@@ -32,7 +28,7 @@ const testDataImg = {
     0xf0,
     0xff,
   ],
-}
+};
 const testDataImg2 = {
   width: 4,
   height: 4,
@@ -54,102 +50,37 @@ const testDataImg2 = {
     0xec,
     0xfc,
   ],
-}
+};
 
-describe('weberSsim', () => {
-  let options: Options
-  let k0Options: Options
+describe("weberSsim", () => {
+  let options: Options;
+  let k0Options: Options;
 
   beforeEach(() => {
-    options = { ...defaults, ssim: 'weber' }
-    k0Options = { ...defaults, ssim: 'weber', k1: 0, k2: 0 }
-  })
+    options = { ...defaults, ssim: "weber" };
+    k0Options = { ...defaults, ssim: "weber", k1: 0, k2: 0 };
+  });
 
   test("should return same results than Webers's implementation", () => {
-    const A = samples['24x18'].gray
-    const B = samples['24x18-degraded'].gray
-    const ssimMap = ssim(A, B, options).ssim_map
-    expect(roundTo(mean2d(ssimMap), 5)).toMatchInlineSnapshot(`0.95763`)
-  })
+    const A = samples["24x18"].gray;
+    const B = samples["24x18-degraded"].gray;
+    const ssimMap = ssim(A, B, options).ssim_map;
+    expect(roundTo(mean2d(ssimMap), 5)).toMatchInlineSnapshot(`0.95763`);
+  });
 
-  test('rolling ssim mssim should be the same as mean2d ssim', () => {
-    const A = sampleCsv.lena
-    const B = sampleCsv.lena02876
-    const { ssim_map: ssimMap, mssim } = ssim(A, B, options)
-    expect(roundTo(mean2d(ssimMap), 5)).toMatchInlineSnapshot(`0.99327`)
-    expect(roundTo(mssim, 5)).toMatchInlineSnapshot(`0.99327`)
-  })
-
-  test("mean2d to match to equal the Welford's mean of the windowSums", () => {
-    const A = sampleCsv.lena
-    const meanAcross = mean2d(A)
-    const sums = windowSums(A, 1)
-    let welfordMean = 0
-    for (let i = 0; i < sums.data.length; ++i) {
-      welfordMean = welfordMean + (sums.data[i] - welfordMean) / (i + 1)
-    }
-    expect(roundTo(welfordMean, 5)).toBe(roundTo(meanAcross, 5))
-  })
-
-  test("matlab averages, variances, covariances should be the same as weber's", () => {
-    const A = testDataImg
-    const B = testDataImg2
-    const windowSize = 2
-    const windowWidth = A.width - windowSize + 1
-    const windowHeight = A.height - windowSize + 1
-    const matlabXAverages = []
-    const matlabYAverages = []
-    const matlabXVariances = []
-    const matlabYVariances = []
-    const matlabCovariances = []
-    for (let y = 0; y < windowHeight; ++y) {
-      for (let x = 0; x < windowWidth; ++x) {
-        const values1 = sub(A, x, windowSize, y, windowSize)
-        const values2 = sub(B, x, windowSize, y, windowSize)
-        const avgX = average(values1.data)
-        const avgY = average(values2.data)
-        const varX = variance(values1.data, avgX)
-        const varY = variance(values2.data, avgY)
-        const covX = covariance(values1.data, values2.data, avgX, avgY)
-        matlabXAverages.push(avgX)
-        matlabYAverages.push(avgY)
-        matlabXVariances.push(varX)
-        matlabYVariances.push(varY)
-        matlabCovariances.push(covX)
-      }
-    }
-    const sumsX = windowSums(A, windowSize)
-    const meansX = Array.from(
-      sumsX.data,
-      (v: number) => v / (windowSize * windowSize)
-    )
-    const variancesX = windowVariance(A, sumsX, windowSize)
-    const sumsY = windowSums(B, windowSize)
-    const meansY = Array.from(
-      sumsY.data,
-      (v: number) => v / (windowSize * windowSize)
-    )
-    const variancesY = windowVariance(B, sumsY, windowSize)
-    const covariances = windowCovariance(A, B, sumsX, sumsY, windowSize)
-    expect(meansX.length).toBe(matlabXAverages.length)
-    expect(meansX.length).toBe(matlabYAverages.length)
-    expect(meansX.length).toBe(matlabXVariances.length)
-    expect(meansX.length).toBe(matlabYVariances.length)
-    expect(meansX.length).toBe(matlabCovariances.length)
-    for (let i = 0; i < meansX.length; ++i) {
-      expect(meansX[i]).toBe(matlabXAverages[i])
-      expect(meansY[i]).toBe(matlabYAverages[i])
-      expect(variancesX.data[i] / 1024).toBe(matlabXVariances[i])
-      expect(variancesY.data[i] / 1024).toBe(matlabYVariances[i])
-      expect(covariances.data[i] / 1024).toBe(matlabCovariances[i])
-    }
-  })
+  test("rolling ssim mssim should be the same as mean2d ssim", () => {
+    const A = sampleCsv.lena;
+    const B = sampleCsv.lena02876;
+    const { ssim_map: ssimMap, mssim } = ssim(A, B, options);
+    expect(roundTo(mean2d(ssimMap), 5)).toMatchInlineSnapshot(`0.99329`);
+    expect(roundTo(mssim, 5)).toMatchInlineSnapshot(`0.99329`);
+  });
 
   test("should return NaN with Weber's implementation when k1 / k2 are 0", () => {
-    const A = samples['24x18'].gray
-    const B = samples['24x18-degraded'].gray
-    const { ssim_map: ssimMap, mssim } = ssim(A, B, k0Options)
-    expect(mssim).toBeNaN()
-    expect(mean2d(ssimMap)).toBeNaN()
-  })
-})
+    const A = samples["24x18"].gray;
+    const B = samples["24x18-degraded"].gray;
+    const { ssim_map: ssimMap, mssim } = ssim(A, B, k0Options);
+    expect(mssim).toBeNaN();
+    expect(mean2d(ssimMap)).toBeNaN();
+  });
+});
