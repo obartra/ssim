3be9c57f44d0efdd2beb3c62fb9acd927d27b1cb
Oscar <obartra@gmail.com>
2020-10-09 18:06:09 -0400

    perf(ssim): improve rgb and weber speed (#284)
    Weber algorithm improvements:
    - No interim rounding
    - No division
    - Removed branching
    - Keeps all numbers as floating points at the end
    
    RGB to Grayscale improvements:
    - Makes integer rgb calculation the default.
    - Old behavior can be restored by setting the option `rgb2grayVersion`
      to 'original'
    - Value changes are ~±0.001
    
    Closes #276, closes #277, closes #278, closes #279, closes #281
    
    BREAKING CHANGE

--- spec/e2e/ivc.spec.ts
+++ spec/e2e/ivc.spec.ts
@@ -15,7 +15,10 @@ describe('IVC', () => {
     const expected = await getJSONScores(scores, path, 'bmp')
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'fast' })
+        const { mssim } = ssim(reference, file, {
+          ssim: 'fast',
+          rgb2grayVersion: 'original',
+        })
 
         return [key, roundTo(mssim, 3)]
       })
@@ -35,7 +38,10 @@ describe('IVC', () => {
     const start = new Date().getTime()
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'weber' })
+        const { mssim } = ssim(reference, file, {
+          ssim: 'weber',
+          rgb2grayVersion: 'original',
+        })
         return [key, roundTo(mssim, 3)]
       })
       .reduce(
@@ -78,7 +84,10 @@ describe('IVC', () => {
     const start = new Date().getTime()
     const results = Object.entries(expected)
       .map(([key, { file, reference }]): [string, number] => {
-        const { mssim } = ssim(reference, file, { ssim: 'bezkrovny' })
+        const { mssim } = ssim(reference, file, {
+          ssim: 'bezkrovny',
+          rgb2grayVersion: 'original',
+        })
         return [key, roundTo(mssim, 3)]
       })
       .reduce(
@@ -111,5 +120,107 @@ describe('IVC', () => {
     expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0155`)
     expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000153`)
     expect(results).toEqual(bezkrovnyScores as MSSIMValues)
+  }, 70000)
+
+  it('should match stored mssims (ssim fast) - rgb int', async () => {
+    const expected = await getJSONScores(scores, path, 'bmp')
+    const results = Object.entries(expected)
+      .map(([key, { file, reference }]): [string, number] => {
+        const { mssim } = ssim(reference, file, { ssim: 'fast' })
+
+        return [key, roundTo(mssim, 3)]
+      })
+      .reduce(
+        (acc, [key, result]) => ({
+          ...acc,
+          [key]: result,
+        }),
+        {} as MSSIMValues
+      )
+
+    expect(scores as MSSIMValues).toMatchSnapshot()
+  }, 500000)
+
+  it('should match stored mssims (weber) - rgb int', async () => {
+    const expected = await getJSONScores(weberScores, path, 'bmp')
+    const start = new Date().getTime()
+    const results = Object.entries(expected)
+      .map(([key, { file, reference }]): [string, number] => {
+        const { mssim } = ssim(reference, file, { ssim: 'weber' })
+        return [key, roundTo(mssim, 3)]
+      })
+      .reduce(
+        (acc, [key, result]) => ({
+          ...acc,
+          [key]: result,
+        }),
+        {} as MSSIMValues
+      )
+
+    const end = new Date().getTime()
+    const referenceScores = scores as MSSIMValues
+    const newV: any = {}
+    let newMean = 0
+    let newS = 0
+    for (let score in referenceScores) {
+      const refVal = referenceScores[score]
+      const weberNewVal = results[score]
+      const distWeberNew = Math.abs(refVal - weberNewVal)
+      newV[score] = {
+        mssim: weberNewVal,
+        distance: distWeberNew,
+        ref: refVal,
+      }
+      const newMeanR =
+        newMean + (distWeberNew - newMean) / Object.keys(newV).length
+      newS = newS + (distWeberNew - newMean) * (distWeberNew - newMeanR)
+      newMean = newMeanR
+    }
+
+    const newVar = newS / (Object.keys(newV).length - 1)
+
+    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0203`)
+    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000212`)
+    expect(results).toMatchSnapshot()
+  }, 70000)
+
+  it('should match stored mssims (bezkrovny) - rgb int', async () => {
+    const expected = await getJSONScores(weberScores, path, 'bmp')
+    const start = new Date().getTime()
+    const results = Object.entries(expected)
+      .map(([key, { file, reference }]): [string, number] => {
+        const { mssim } = ssim(reference, file, { ssim: 'bezkrovny' })
+        return [key, roundTo(mssim, 3)]
+      })
+      .reduce(
+        (acc, [key, result]) => ({
+          ...acc,
+          [key]: result,
+        }),
+        {} as MSSIMValues
+      )
+    const end = new Date().getTime()
+
+    const referenceScores = scores as MSSIMValues
+    const newV: any = {}
+    let newMean = 0
+    let newS = 0
+    for (let score in referenceScores) {
+      const refVal = referenceScores[score]
+      const newVal = results[score]
+      const distNew = Math.abs(refVal - newVal)
+      newV[score] = {
+        mssim: newVal,
+        distance: distNew,
+        ref: refVal,
+      }
+      const meanR = newMean + (distNew - newMean) / Object.keys(newV).length
+      newS = newS + (distNew - newMean) * (distNew - meanR)
+      newMean = meanR
+    }
+    const newVar = newS / (Object.keys(newV).length - 1)
+    expect(roundTo(newMean, 4)).toMatchInlineSnapshot(`0.0156`)
+    expect(roundTo(newVar, 6)).toMatchInlineSnapshot(`0.000154`)
+    expect(results).toMatchSnapshot()
   }, 70000)
 })
